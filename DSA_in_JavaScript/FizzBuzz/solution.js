392fa76bd1ec6e9d7ee2465d0357d70733d40da1
Pankaj Kumar Ram <pankajkr885@gmail.com>
2025-11-25 16:45:38 +0530

    Update solution.js

--- DSA_in_JavaScript/FizzBuzz/solution.js
+++ DSA_in_JavaScript/FizzBuzz/solution.js
@@ -1,2 +1,14 @@
-console.log("FizzBuzz Solution Loaded");
 
+let N=100;
+for(let i=1; i<=N; i++){
+
+    if(i%3 ===0 && i%5 === 0){
+        console.log("FizzBuzz");
+    } else if(i%3 === 0){
+        console.log("Fizz");
+    } else if(i%5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
