29330edd66a928903bd0933c0dfa630378cbf259
Pankaj Kumar Ram <pankajkr885@gmail.com>
2025-11-26 15:37:58 +0530

    Update Solution.js

--- DSA_in_JavaScript/02_Check for prime/Solution.js
+++ DSA_in_JavaScript/02_Check for prime/Solution.js
@@ -19,14 +19,22 @@ if (isPrime(number)) {
 
 
 
-`Every composite number N has factor pairs (a, b) such that a × b = N.
+const explantion = `
+──────────────────────────────────────────
+           CODE EXPLANATION
+──────────────────────────────────────────
+A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers.
+To determine if a number N is prime, we only need to check for factors up to √N.
+This is because of the following reasoning:
+
+Every composite number N has factor pairs (a, b) such that a × b = N.
 In every pair, one factor must be ≤ √N and the other ≥ √N.
 So if N has any divisor at all, we will always find the smaller one by the time we reach √N.
 If no number ≤ √N divides N, then no number > √N will divide it either.
-Therefore, checking up to √N guarantees correctness and reduces complexity from O(N) to O(√N).`
+Therefore, checking up to √N guarantees correctness and reduces complexity from O(N) to O(√N).
 
 
-`This function checks whether a number is prime using an optimized approach.
+This function checks whether a number is prime using an optimized approach.
 I first handle edge cases like numbers ≤ 1, the number 2, and even numbers.
 Then I check divisors only up to √n, because factors come in pairs, and if a number has a divisor 
 greater than √n, its paired divisor must be smaller than √n.
