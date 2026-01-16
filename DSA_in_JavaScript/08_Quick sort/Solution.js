9b68ffebfad23e619713de391a1cca3b7d40c680
Pankaj Kumar Ram <pankajkr885@gmail.com>
2026-01-16 12:21:33 +0530

    revise

--- DSA_in_JavaScript/08_Quick sort/Solution.js
+++ DSA_in_JavaScript/08_Quick sort/Solution.js
@@ -180,3 +180,26 @@ return [...quickSort(left), pivot, ...quickSort(right)];
 
 
 `
+
+
+function quickSortAlg(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let pivot = arr[arr.length - 1];
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSortAlg(left), pivot, ...quickSortAlg(right)];
+}
+
+console.log(quickSortAlg(array));
